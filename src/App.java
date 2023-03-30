8e01e5e793741802cd12dafad3d8d2a66e5946db
Ariwibowogithub <arie35411@gmail.com>
2023-03-30 22:16:46 +0700

    update baru

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Tugas Praktikum Yang Ke 4 || Ari Wibowo || A11.2021.13297");
     }
 }
